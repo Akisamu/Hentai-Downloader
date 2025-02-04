d5537110b336465036593a483e5fb7bb54363d21
Akisamu <akisamu0901@gmail.com>
2025-02-04 23:12:39 +0900

    fix:main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from modules import eromanga, nhentai
+from modules.eromanga import get_images
 from modules.ptf.i2p import I2P
 
 '''
 param url: 'https://eromanga-show.com/viewer?articleId=n&page=n'
 '''
 urls = [
-    '233'
+
 ]
 
 """
@@ -13,15 +14,21 @@ urls = [
         'name': '',
         'final': 1,
         'id': 1,
-        'format': 'jpg'
+        'format': 'webp'
     }
 """
 infos = [
     {
-        'name': '[あくた～ (木家マユ)] イクのガマンゲーム!風紀委員長悶絶クリ責め地獄 [英訳]',
-        'final': 3,
-        'id': 545332,
-        'format': 'jpg'
+        'name': '(C105) [えだまめ亭 (うこ)] 先生は私を××してくれない (ブルーアーカイブ) [中国翻訳]',
+        'final': 98,
+        'id': 583898,
+        'format': 'webp'
+    },
+    {
+        'name': '(C105) [えだまめ亭 (うこ)] 先生は私を××してくれない (ブルーアーカイブ) [中国翻訳]',
+        'final': 98,
+        'id': 583898,
+        'format': 'webp'
     }
 ]
 
@@ -36,8 +43,15 @@ if __name__ == '__main__':
     if is_empty(infos) is False:
         for info in infos:
             image = nhentai.get_images(info)
-            p1 = I2P(images=image, pdf_name=info['name'])
+            p1 = I2P(images=image, pdf_name=info['name'], is_compress=False)
             p1.convert_images_to_pdf()
+
+    if is_empty(urls) is False:
+        for url in urls:
+            image = get_images(url=url['url'])
+            p2 = I2P(images=image, pdf_name=url['name'], is_compress=False)
+            p2.convert_images_to_pdf()
+
 
 
 
